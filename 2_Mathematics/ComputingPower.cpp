dd2c523efbe620163303048cbc4174f0f31c08a4
hnmali <hsmalisirohi@gmail.com>
2024-06-03 18:04:54 +0530

    Create ComputingPower.cpp

---
+++ 2_Mathematics/ComputingPower.cpp
@@ -0,0 +1,21 @@
+//You have to create power function which calculates a ^ b
+#include<iostream>
+using namespace std;
+
+int power(int a, int b) {
+    if (b == 0)
+        return 1;
+    int temp = power(a,b/2);
+    temp *= temp;
+    if (n%2 == 0)
+        return temp;
+    else
+        return temp*a;
+}// TC: O(log n) ; Aux. Space: O(log n)
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout << power(a,b);
+    return 0;
+}
